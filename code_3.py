1d8d1d7c103a469efb6e078443c980b83278432a
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2024-05-29 22:13:34 -0600

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+    data.forEach(item => {
+}
+
+    for (let i = 0; i < 10; i++) {
+function main() {
+    const data = generateRandomData();
+}
+    });
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+main();
+    }
+        console.log("Random Number: " + item);
+    return data;
+
+    const data = [];
